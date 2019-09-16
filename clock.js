9b16285ac048338fbe2b7f90edd05f67370281d4
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-09-15 17:07:43 -0700

    finished

--- clock.js
+++ clock.js
@@ -1,29 +1,34 @@
-var hours = 1;
-var seconds =1;
-var minutes =1;
+var second = document.getElementById('second');
+var hour = document.getElementById('hour');
+var minute = document.getElementById('minute');
 
 
-var s = document.getElementById('second');
-var m = document.getElementById('minute');
-var h = document.getElementById('hand');
+var h = 0;
+var s = 0;
+var m = 0;
 
 
 
-setInterval(secondsRotation, 1000);
-setInterval(minutesRotation, 60000);
-setInterval(hoursRotation, 360000);
+setInterval(function(){
+ 	s += 6;
+	second.style.transform = "rotate(" + s + "deg)"; 
+	s++;
+	
+ }, 1000);
 
-function secondsRotation(){
-	var secondsDegrees = seconds * 6
-	s.style.transform = "rotate(" + secondsDegrees + "deg)"; seconds++;
-}
+setInterval(function(){
+ 	m += 6;
+	minute.style.transform = "rotate(" + m + "deg)"; 
+	m++;
+	console.log(minute);
+ }, 52000);
+setInterval(function(){
+ 	h += 6;
+	hour.style.transform = "rotate(" + h + "deg)"; 
+	h++;
+	console.log(minute);
+ }, 360000);
 
-function minutesRotation(){
-	var minutesDegrees = minutes * 6
-	m.style.transform = "rotate(" + minutesDegrees + "deg)"; minutes++;
-}
-
-function hoursRotation(){
-	var minutesDegrees = hours * 30
-	h.style.transform = "rotate(" + hoursDegrees + "deg)"; hours++;
+function getTime(){
+	var now = new Date();
 }
